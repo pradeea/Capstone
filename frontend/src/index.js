cb3ca0311b07a0a17cfe86e3c51ab9645d47b9e4
pradeea <pradeeakumar@gmail.com>
2024-02-02 16:58:21 -0500

    store wraped

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./store";
+import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <Toaster/>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
+
+
