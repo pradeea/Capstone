0ad0edbf9e4c6e98d2bacb4cdebacecb1dca2318
Sairam-Pagadala <150365790+Sairam-Pagadala@users.noreply.github.com>
2024-04-15 00:39:57 -0400

    Implemented Message

--- frontend/src/Components/Admin/components/chat/Message.jsx
+++ frontend/src/Components/Admin/components/chat/Message.jsx
@@ -2,16 +2,18 @@ import { Avatar } from '@mui/material'
 import React from 'react'
 
 const Message = () => {
+  // Dummy data
+  const dummyAvatarText = 'JD'; // Initials for a user's name
+  const dummyMessageText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla fermentum metus id felis elementum, vitae vestibulum libero interdum.'; // Placeholder message text
+
   return (
     <div className='message owner'>
-        <div className="messageinfo">  
-        <Avatar sx={{ width: 30, height: 30 }}>N</Avatar> 
-  
-        
-        </div>
-        <div className="messagecontent">
-            <p className='texts'>hello</p>
-        </div>
+      <div className="messageinfo">  
+        <Avatar sx={{ width: 30, height: 30 }}>{dummyAvatarText}</Avatar> 
+      </div>
+      <div className="messagecontent">
+        <p className='texts'>{dummyMessageText}</p>
+      </div>
     </div>
   )
 }
