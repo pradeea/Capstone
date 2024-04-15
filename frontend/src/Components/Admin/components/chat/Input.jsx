02e239728a7ca85616eeb9bb4e34ee0af368a51c
Sairam-Pagadala <150365790+Sairam-Pagadala@users.noreply.github.com>
2024-04-15 00:39:44 -0400

    Implemented Input for Chat

--- frontend/src/Components/Admin/components/chat/Input.jsx
+++ frontend/src/Components/Admin/components/chat/Input.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Input = () => {
+  const chatInput = () =>{
+    console.log(`chat msg test`)
+  }
   return (
     <div className='input'>
         <input type="text" className='inputfield' placeholder='write a message' />
