d4aab6e91142b2375ed2f97037c8d7fa7aad72ee
srirammartha <srirammartha36@gmail.com>
2024-04-14 23:57:36 -0400

    fixed bugs

--- frontend/src/Components/Admin/Dashboard.jsx
+++ frontend/src/Components/Admin/Dashboard.jsx
@@ -14,6 +14,7 @@ import ChatDashboard from './components/chat/ChatDashboard';
 
 const Dashboard = () => {
   const [theme, colorMode] = useMode();
+  console.log(`test`);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
